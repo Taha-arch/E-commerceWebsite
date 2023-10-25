eb3a8cd35258f0812ac016aadbdacf908645adda
Amine ELBOUZYDY <amineelbouzidi36@gmail.com>
2023-10-25 10:09:31 +0100

    Order APIs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const costumerRoutes = require('./routes/customerRoutes');
+
+
 const app = express();
 const dotenv = require('dotenv').config() ;
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT;
+const connectDb = require('./config/database.js')
+connectDb();
+
+
+
+
+const costumerRoutes = require('./routes/customerRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+
+    app.use(express.json());
+    app.use(costumerRoutes);
+    app.use(orderRoutes);
+
+
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -13,11 +29,11 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     console.error('Connection to MongoDB failed:', err);
     });
 
-    app.use(express.json());
-    app.use(costumerRoutes);
+
+    
     
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
