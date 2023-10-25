04886ab94a251f2c1489a3367fb43eb7599bfa2b
RajaeKacemi <rajaekacemi@gmail.com>
2023-10-25 15:29:50 +0100

    add authorization product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const multer = require('multer');
 const {addProduct, getAllProducts, searchProducts, getProduct, updateProduct, deleteProduct} = require('../controllers/productController.js');
+const {authorization, checkAdminOrManager, checkAdmin} = require('../middleware/authMiddleware');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,15 +16,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-router.post('/products', upload.single('image'), addProduct);
+router.post('/products', checkAdminOrManager, upload.single('image'), addProduct);
 router.get('/products', (req, res, next) => {
     return (Object.keys(req.query).length > 0 ) ? (searchProducts(req, res, next)) : (getAllProducts(req, res, next));
 });
 
 router.get('/products/:id', getProduct);
-router.patch('/products/:id', updateProduct);
+router.patch('/products/:id', checkAdminOrManager, updateProduct);
 router.get('/products/:id', getProduct);
-router.delete('/products/:id', deleteProduct);
+router.delete('/products/:id', checkAdminOrManager, deleteProduct);
 
 
 module.exports = router;
